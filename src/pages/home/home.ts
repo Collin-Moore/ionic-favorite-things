2a0fbf32fbfba5f0600cf83995ca1761c2008534
Collin Moore <collin.moore96@gmail.com>
2017-08-04 18:35:55 -0500

    added simple firebase push and pull

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from "angularfire2/database";
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  public favoriteColor: string;
 
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+
+  }
+  
+  ngOnInit(): void {
+    firebase.database().ref("/color").on("value", (snapShot: firebase.database.DataSnapshot) => {
+      this.favoriteColor = snapShot.val();
+    });
   }
 
   setColor(selectedColor: string): void {
     console.log('TODO: set color:', selectedColor);
+    firebase.database().ref("/color").set(selectedColor);
   }
 }
